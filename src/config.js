9a5bc863b1ff410fa0b93199df76bc538abf9108
victor de souza magaldi <victmagaldi@hotmail.com>
2022-04-17 18:25:32 -0300

    commit initial

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// using module.exports here so gatsby-config can read it
+module.exports = {
+  // TODO: Replace next line with your own public API key
+  builderAPIKey: '59bb518773c14842921abe05d5e2bee3'
+};
